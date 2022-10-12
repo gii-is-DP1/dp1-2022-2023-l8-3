10655017da83462688e609a0dfea29e29c8a685f
manortgar <mog2000tb@gmail.com>
2022-10-12 14:17:05 +0200

    PlayerRepository

--- src/main/java/org/springframework/samples/petclinic/jugador/PlayerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/jugador/PlayerRepository.java
@@ -1,7 +1,19 @@
 package org.springframework.samples.petclinic.jugador;
 
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface PlayerRepository extends CrudRepository<Jugador, Integer>{
+	
+	@Query("SELECT jugador FROM Jugador jugador")
+	List<Jugador> findAll() throws DataAccessException;
+	
+	
+	@Query("SELECT jugador FROM Jugador WHERE jugador.id = :id")
+	Jugador findById(@Param("Id") int id); 
 
 }
