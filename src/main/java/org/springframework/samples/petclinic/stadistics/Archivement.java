3de7e2e0c462521c7e3a0d8e439b7329374b7d75
manortgar <mog2000tb@gmail.com>
2022-10-20 13:24:33 +0200

    Create Archivement.java

---
+++ src/main/java/org/springframework/samples/petclinic/stadistics/Archivement.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic.stadistics;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Archivement {
+
+}
