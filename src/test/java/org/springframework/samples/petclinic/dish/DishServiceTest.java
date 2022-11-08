f3ec9384fd76f9b2213f0e5acb0598f8d791166b
josemicpy <josibocon@alum.us.es>
2022-11-08 19:14:28 +0100

    Repo, controlador y servicio de Disco e Invitacion.

---
+++ src/test/java/org/springframework/samples/petclinic/dish/DishServiceTest.java
@@ -0,0 +1,24 @@
+package org.springframework.samples.petclinic.dish;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.petclinic.disco.DishService;
+import org.springframework.stereotype.Service;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class DishServiceTest {
+
+	@Autowired
+	private DishService dishService;
+
+	@Test
+	public void testCountWithInitialData() {
+		int count = dishService.dishCount();
+		assertEquals(count, 7);
+	}
+
+}
